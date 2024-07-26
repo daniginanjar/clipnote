4c1f7d922bb51c5ff727937b20b9e45c80205dfb
daniginanjargarut <daniginanjargarut@gmail.com>
2024-07-26 16:16:36 +0700

    Commit dari kantor siang hari 2024-07-26 part 2

--- application/views/_partials/navbar.php
+++ application/views/_partials/navbar.php
@@ -1,6 +1,5 @@
 <nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container-fluid">
-      
+  <div class="container-fluid">
 
       <?php 
       
@@ -8,30 +7,57 @@
 
       ?>
 
-      <div class="navbar-header">        
-        <a class="navbar-brand" href="<?php echo base_url(); ?>clipnote"><span class="glyphicon glyphicon-home"></span> Clipnote</a>
-      </div>
-
-      <ul class="nav navbar-nav navbar-right">        
-        <li><a href="#"><span class="glyphicon glyphicon-user"></span> <?php echo $this->session->userdata('name'); ?></a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-cog"></span> Password Change</a></li> 
-        <li><a href="<?php echo base_url(); ?>user"><span class="glyphicon glyphicon-user"></span>Admin Page</a></li>                
-        <li><a href="<?php echo base_url(); ?>auth/logout"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="<?php echo base_url(); ?>clipnote"><span class="glyphicon glyphicon-home"></span> Clipnote</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">        
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Admin Page<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="<?php echo base_url(); ?>user">User Management</a></li>
+            <li><a href="#">Authority Management</a></li>            
+          </ul>
+        </li>
+        <li><a href="<?php echo base_url(); ?>clipnote">Notes</a></li>        
       </ul>
+      <ul class="nav navbar-nav navbar-right">                
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-user"></span> <?php echo $this->session->userdata('name'); ?> <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#"><span class="glyphicon glyphicon-cog"></span> Password Change</a></li>                      
+            <li><a href="<?php echo base_url(); ?>auth/logout"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>           
+          </ul>
+        </li>
+        
+      </ul>
+    </div>
 
       <?php
 
         } else {
 
       ?>
-
-      <div class="navbar-header">
-        <a class="navbar-brand" href="<?php echo base_url(); ?>auth"><span class="glyphicon glyphicon-home"></span> Clipnote</a>
-      </div>
-
-      <ul class="nav navbar-nav navbar-right">               
-        <li><a href="<?php echo base_url(); ?>auth"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="<?php echo base_url(); ?>clipnote"><span class="glyphicon glyphicon-home"></span> Clipnote</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">      
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="<?php echo base_url(); ?>clipnote"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
       </ul>
+    </div>
 
       <?php
         
@@ -39,11 +65,9 @@
       
       ?>
 
-    </div>
-  </nav>
+  </div>
+</nav>
 
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
+<br>
+<br>
+
